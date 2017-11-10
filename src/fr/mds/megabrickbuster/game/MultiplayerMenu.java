00dbd36c5729bfc4f68d55d46308dc85d021ea99
Florentin <Florentin@192.168.1.22>
2017-11-10 17:26:15 +0100

    MultiplayerMenu improvement

--- src/fr/mds/megabrickbuster/game/MultiplayerMenu.java
+++ src/fr/mds/megabrickbuster/game/MultiplayerMenu.java
@@ -2,12 +2,11 @@ package fr.mds.megabrickbuster.game;
 
 
 import org.lwjgl.input.Mouse;
-import org.newdawn.slick.Font;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.gui.TextField;
@@ -19,7 +18,7 @@ public class MultiplayerMenu extends BasicGameState {
 	
 	private static int STATE = 5;
 
-	MenuButton client, server;
+	MenuButton clientOk, serverOk;
 	TextField clientTextField, serverTextField;
 	
 	
@@ -28,20 +27,27 @@ public class MultiplayerMenu extends BasicGameState {
 	
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		client = new MenuButton(220, 100, 100, 25, "client");
-		server = new MenuButton(220, 200, 100, 25, "server");
-		clientTextField = new TextField(arg0,arg0.getDefaultFont(), 170, 150, 300 , 25);
-		serverTextField = new TextField(arg0, arg0.getDefaultFont(), 170, 250, 300 , 25);
-		clientTextField.setText("Enter Ip adress of the server");
-		serverTextField.setText("Show me my Ip adress");
-
+		clientOk = new MenuButton(230, 220, 100, 25, "Search");
+		serverOk = new MenuButton(230, 420, 100, 25, "Show my Ip");
+		clientTextField = new TextField(arg0,arg0.getDefaultFont(), 140, 150, 270 , 25);
+		serverTextField = new TextField(arg0, arg0.getDefaultFont(), 180, 350, 200 , 25);
+		clientTextField.setText("Here");
+		clientTextField.setBorderColor(Color.red);
+		clientTextField.setBackgroundColor(Color.gray);
+		serverTextField.setText("IPV4");
+		serverTextField.setBorderColor(Color.red);
+		serverTextField.setBackgroundColor(Color.gray);
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
 		arg2.drawString(BrickBusterLauncher.name, 210, 30);
-		client.render(arg2);
-		server.render(arg2);
+		clientOk.render(arg2);
+		serverOk.render(arg2);
+		arg2.drawRect(130, 100, 300, 150);
+		arg2.drawRect(130, 300, 300, 150);
+		arg2.drawString("Enter Ip adress of the server", 140, 110);
+		arg2.drawString("Show me my Ip adress", 180, 310);
 		clientTextField.render(arg0, arg2);
 		serverTextField.render(arg0, arg2);
 	}
@@ -51,14 +57,18 @@ public class MultiplayerMenu extends BasicGameState {
 		Input input = arg0.getInput();
 		int posX = Mouse.getX();
 		int posY = Mouse.getY();
-		boolean isClient = (posX > 220  && posX < 320) && (posY < BrickBusterLauncher.WINDOW_SIZE_X - 120 && posY > BrickBusterLauncher.WINDOW_SIZE_Y - 145);
-		boolean isServer = (posX > 220  && posX < 320) && (posY < BrickBusterLauncher.WINDOW_SIZE_X - 150 && posY > BrickBusterLauncher.WINDOW_SIZE_Y - 175);
+		boolean isClientSearch = (posX > 230  && posX < 330) && (posY < BrickBusterLauncher.WINDOW_SIZE_X - 220 && posY > BrickBusterLauncher.WINDOW_SIZE_Y - 245);
+		boolean isServerSearch = (posX > 230  && posX < 330) && (posY < BrickBusterLauncher.WINDOW_SIZE_X - 420 && posY > BrickBusterLauncher.WINDOW_SIZE_Y - 445);
+		boolean isClientText = (posX > 140  && posX < 410) && (posY < BrickBusterLauncher.WINDOW_SIZE_X - 150 && posY > BrickBusterLauncher.WINDOW_SIZE_Y - 175);
 		if(input.isMouseButtonDown(0)) {
-			if (isClient) {
-				
+			if (isClientSearch) {
+				//a faire
 			}
-			else if (isServer) {
-				
+			else if (isServerSearch) {
+				// A faire
+			}
+			else if (isClientText) {
+				// A faire
 			}
 		}		
 	}
